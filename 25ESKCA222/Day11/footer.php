d75dc1356042dc34d9f658704b0efc7e7650513e
siyaagarwall <siyaagarwall16@gmail.com>
2026-07-08 23:12:47 +0530

    Create footer.php

---
+++ 25ESKCA222/Day11/footer.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-white text-center py-3 mt-5">
+    <p class="mb-0">&copy;<?php echo date("Y"); ?>My website.all rights reserved.</p>
+</footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
